868b62be236d0601454c11096990e51e6dd3c159
Rallve <lucas.liedgren@gmail.com>
2023-05-05 09:45:36 +0200

    skapade app.js och server.js

---
+++ app.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const nunjucks = require('nunjucks');
+const session = require('express-session');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app,
+});
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    LoggedIn: false,
+  }));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+module.exports = app;
